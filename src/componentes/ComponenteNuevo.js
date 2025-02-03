ba0c2047c5067d82fb4f94cb7105eabe2313c5a9
Fernando <perezmartin.fernando@gmail.com>
2025-02-03 14:41:46 +0100

    Añadir estilos en línea a ComponenteNuevo

--- src/componentes/ComponenteNuevo.js
+++ src/componentes/ComponenteNuevo.js
@@ -1,4 +1,7 @@
-// src/components/ComponenteNuevo.js
+
 export default function ComponenteNuevo() {
     return <div>Nuevo componente en desarrollo</div>;
+  }
+  export default function ComponenteNuevo() {
+    return <div style={{ color: 'blue' }}>Nuevo componente en desarrollo</div>;
   }
